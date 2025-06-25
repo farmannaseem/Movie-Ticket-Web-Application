d3cf61ff226054c828273df5ffa85cae2f06ea6a
farmannaseem <farmannaseem789@gmail.com>
2025-06-25 20:47:44 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Movie-ticket', // ← must match your repo name
+  base: '/Movie-ticket/', // ← must match your repo name
   plugins: [react()],
 });
